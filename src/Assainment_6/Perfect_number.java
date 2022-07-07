6e2c881356d1d8cef99006e96aa9ca5d82c944c2
adithya89780 <adithyasachin123@gmail.com>
2022-07-07 22:58:28 +0530

    Assaignmnet_day_6

--- src/Assainment_6/Perfect_number.java
+++ src/Assainment_6/Perfect_number.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Perfect_number {
     public static void main(String[] args) {
 
-    int i, Number, Sum = 0 ;
-    Scanner sc = new Scanner(System.in);
-		System.out.println("\n Please Enter any Number: ");
-    Number = sc.nextInt();
+        int n,  Sum = 0 ;
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter a Number: ");
+        n = sc.nextInt();
 
-		for(i = 1 ; i < Number ; i++) {
-        if(Number % i == 0)  {
+		for(int i = 1 ; i < n ; i++) {
+        if(n % i == 0)  {
             Sum = Sum + i;
         }
     }
-		if (Sum == Number) {
-        System.out.format("\n% d is a Perfect Number", Number);
+		if (Sum == n) {
+        System.out.format(n+" is a Perfect Number");
     }
 		else {
-        System.out.format("\n% d is NOT a Perfect Number", Number);
+        System.out.format(n+" is not a Perfect Number");
         }
     }
 }
